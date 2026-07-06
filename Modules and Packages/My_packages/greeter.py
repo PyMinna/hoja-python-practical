9ec50290731c18bb67d3e6567475a165ad3e75c0
PyMinna <kadeejaminnna@gmail.com>
2026-07-06 12:34:03 +0530

    package import example

---
+++ Modules and Packages/My_packages/greeter.py
@@ -0,0 +1,2 @@
+def say_hello(name):
+    return f"helloo {name}"
---
+++ Modules and Packages/import_package.py
@@ -0,0 +1,12 @@
+from My_packages import calculator,greeter
+
+print(calculator.add(100,200))
+print(calculator.subtract(300,50))
+
+print(greeter.say_hello("Minna"))
+
+added = calculator.add(150,250)
+print(added)
+
+sub = calculator.subtract(added,50)
+print(sub)
