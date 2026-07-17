f5c0cb4571abff28340056a462027a9ab490b1e8
PyMinna <kadeejaminnna@gmail.com>
2026-07-17 11:20:28 +0530

    list comprehension examples

--- Comprehensions/list_comprehension.py
+++ Comprehensions/list_comprehension.py
@@ -4,6 +4,14 @@ for x in range(1,6):
     square.append(x ** 2)
 print(square)
 
-#list comprehension
+#list comprehension - print square
 square_comp = [x ** 2 for x in range(1,6)]
 print(square_comp)
+
+#even numbers
+even_numbers = [x for x in range(1,10+1) if x % 2 == 0]
+print(even_numbers)
+
+#string
+letters = [ch.upper() for ch in "helloo"]
+print(letters)
